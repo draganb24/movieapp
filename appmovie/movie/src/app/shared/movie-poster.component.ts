8566f08d028e12fb3ba933b4e80e04a181e02541
draganb24 <nemanjabjelica45@gmail.com>
2024-02-07 19:22:04 +0100

    @defer and lazy load

--- appmovie/movie/src/app/shared/movie-poster.component.ts
+++ appmovie/movie/src/app/shared/movie-poster.component.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
     <img
       [src]="getImageUrl()"
       class="rowimg largeposter"
+      loading="lazy"
       alt="..."
       [routerLink]="['/movie', movie.id]" />
   `,
